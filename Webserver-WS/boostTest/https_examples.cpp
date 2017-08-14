9e2245cbe9af8f4a63d1d85c2a1373002554bfba
Xin Men <menx@vmware.com>
2017-08-14 11:45:54 +0800

    support to transfer native js file to the client

--- Webserver-WS/boostTest/https_examples.cpp
+++ Webserver-WS/boostTest/https_examples.cpp
@@ -13,13 +13,32 @@ using namespace std;
 typedef SimpleWeb::SocketServer<SimpleWeb::WSS> WssServer;
 typedef SimpleWeb::Server<SimpleWeb::HTTPS> HttpsServer;
 
+// define this class to send files to client more convenient
+class FileServer {
+public:
+	static void read_and_send(const shared_ptr<HttpsServer::Response> &response, const shared_ptr<ifstream> &ifs) {
+		// Read and send 128 KB at a time
+		static vector<char> buffer(131072); // Safe when server is running on one thread
+		streamsize read_length;
+		if ((read_length = ifs->read(&buffer[0], static_cast<streamsize>(buffer.size())).gcount()) > 0) {
+			response->write(&buffer[0], read_length);
+			if (read_length == static_cast<streamsize>(buffer.size())) {
+				response->send([response, ifs](const SimpleWeb::error_code &ec) {
+					if (!ec)
+						read_and_send(response, ifs);
+					else
+						cerr << "Connection interrupted" << endl;
+				});
+			}
+		}
+	}
+};
+
 int main() {
   // HTTPS-server at port 8080 using 1 thread
-  // Unless you do more heavy non-threaded processing in the resources,
-  // 1 thread is usually faster than several threads
   HttpsServer httpsServer("server.crt", "server.key");
   httpsServer.config.port = 8080;
-  // WSS server at port 443
+  // WSS server at port 443 using 1 thread
   WssServer wssServer("server.crt", "server.key");
   wssServer.config.port = 8001;
 
@@ -27,103 +46,69 @@ int main() {
   // Will respond with content in the web/-directory, and its subdirectories.
   // Default file: index.html
   // Can for instance be used to retrieve an HTML 5 client that uses REST-resources on this server
-  httpsServer.default_resource["GET"] = [](shared_ptr<HttpsServer::Response> response, shared_ptr<HttpsServer::Request> request) {
+  httpsServer.resource["^/$"]["GET"] = [](shared_ptr<HttpsServer::Response> response, shared_ptr<HttpsServer::Request> request) {
     try {
-		//ifstream html("device-share.html");
-		string path = "device-share.html";
-        SimpleWeb::CaseInsensitiveMultimap header;
+		
+		  string path = "device-share.html";
+          SimpleWeb::CaseInsensitiveMultimap header;
 
-//    Uncomment the following line to enable Cache-Control
-//    header.emplace("Cache-Control", "max-age=86400");
+		  auto ifs = make_shared<ifstream>();
+		  ifs->open(path, ifstream::in | ios::binary | ios::ate);
 
-#ifdef HAVE_OPENSSL
-//    Uncomment the following lines to enable ETag
-//    {
-//      ifstream ifs(path.string(), ifstream::in | ios::binary);
-//      if(ifs) {
-//        auto hash = SimpleWeb::Crypto::to_hex_string(SimpleWeb::Crypto::md5(ifs));
-//        header.emplace("ETag", "\"" + hash + "\"");
-//        auto it = request->header.find("If-None-Match");
-//        if(it != request->header.end()) {
-//          if(!it->second.empty() && it->second.compare(1, hash.size(), hash) == 0) {
-//            response->write(SimpleWeb::StatusCode::redirection_not_modified, header);
-//            return;
-//          }
-//        }
-//      }
-//      else
-//        throw invalid_argument("could not read file");
-//    }
-#endif
+		  if (*ifs) {
+			  auto length = ifs->tellg();
+			  ifs->seekg(0, ios::beg);
 
-      auto ifs = make_shared<ifstream>();
-      ifs->open(path, ifstream::in | ios::binary | ios::ate);
-
-	  if (*ifs) {
-		  auto length = ifs->tellg();
-		  ifs->seekg(0, ios::beg);
-
-		  header.emplace("Content-Length", to_string(length));
-		  response->write(header);
-	  }
-	  else {
-		  throw invalid_argument("could not read file");
-	  }
-        // Trick to define a recursive function within this scope (for example purposes)
-      class FileServer {
-        public:
-          static void read_and_send(const shared_ptr<HttpsServer::Response> &response, const shared_ptr<ifstream> &ifs) {
-            // Read and send 128 KB at a time
-            static vector<char> buffer(131072); // Safe when server is running on one thread
-            streamsize read_length;
-            if((read_length = ifs->read(&buffer[0], static_cast<streamsize>(buffer.size())).gcount()) > 0) {
-              response->write(&buffer[0], read_length);
-              if(read_length == static_cast<streamsize>(buffer.size())) {
-                response->send([response, ifs](const SimpleWeb::error_code &ec) {
-                  if(!ec)
-                    read_and_send(response, ifs);
-                  else
-                    cerr << "Connection interrupted" << endl;
-                });
-              }
-            }
-          }
-        };
-      FileServer::read_and_send(response, ifs);
-      path = "jquery-3.2.1.min.js";
-	  ifs->open(path, ifstream::in | ios::binary | ios::ate);
-	  if (*ifs) {
-		auto length = ifs->tellg();
-		ifs->seekg(0, ios::beg);
-      }
-      else
-        throw invalid_argument("could not read file");
-	  FileServer::read_and_send(response, ifs);
+			  header.emplace("Content-Length", to_string(length));
+			  response->write(header);
+		  }
+		  else {
+			  throw invalid_argument("could not read file");
+		  }
+    
+          FileServer::read_and_send(response, ifs);   
     }
     catch(const exception &e) {
-      response->write(SimpleWeb::StatusCode::client_error_bad_request, "Could not open path " + request->path + ": " + e.what());
+          response->write(SimpleWeb::StatusCode::client_error_bad_request, "Could not open path " + request->path + ": " + e.what());
     }
   };
 
+  httpsServer.resource["^/jquery-3.2.1.min.js$"]["GET"] = [](shared_ptr<HttpsServer::Response> response, shared_ptr<HttpsServer::Request> request) {
+	  try {
+		  string path = "jquery-3.2.1.min.js";
+		  SimpleWeb::CaseInsensitiveMultimap header;
+		  auto ifs = make_shared<ifstream>();
+		  ifs->open(path, ifstream::in | ios::binary | ios::ate);
+
+		  if (*ifs) {
+			  auto length = ifs->tellg();
+			  ifs->seekg(0, ios::beg);
+
+			  header.emplace("Content-Length", to_string(length));
+			  response->write(header);
+		  }
+		  else {
+			  throw invalid_argument("could not read file");
+		  }
+		  FileServer::read_and_send(response, ifs);
+	  }
+	  catch (const exception &e) {
+		  response->write(SimpleWeb::StatusCode::client_error_bad_request, "Could not open path " + request->path + ": " + e.what());
+	  }
+  };
   httpsServer.on_error = [](shared_ptr<HttpsServer::Request> /*request*/, const SimpleWeb::error_code & /*ec*/) {
     // Handle errors here
+	  cout << "httpsServer crashed !!!" << endl;
   };
 
   thread httpsServer_thread([&httpsServer]() {
     // Start server
-	  //cout << "test" << endl;
 	  httpsServer.start();
   });
 
-  // Wait for server to start so that the client can connect
-  this_thread::sleep_for(chrono::seconds(1));
-
-  // Example 1: echo WebSocket Secure endpoint
-  // Added debug messages for example use of the callbacks
   // Test with the following JavaScript:
   //   var wss=new WebSocket("wss://localhost:8080/echo");
-  //   wss.onmessage=function(evt){console.log(evt.data);};
-  //   wss.send("test");
+
   auto &echo = wssServer.endpoint["^/echo/?$"];
 
   echo.on_message = [](shared_ptr<WssServer::Connection> connection, shared_ptr<WssServer::Message> message) {
@@ -141,9 +126,6 @@ int main() {
 				  "Error: " << ec << ", error message: " << ec.message() << endl;
 		  }
 	  });
-
-	  // Alternatively, using a convenience function:
-	  // connection->send(message_str, [](const SimpleWeb::error_code & /*ec*/) { /*handle error*/ });
   };
 
   echo.on_open = [](shared_ptr<WssServer::Connection> connection) {
@@ -166,9 +148,6 @@ int main() {
 	  // Start WSS-server
 	  wssServer.start();
   });
-
-  // Wait for server to start so that the client can connect
-  this_thread::sleep_for(chrono::seconds(1));
 
   httpsServer_thread.join();
   wssServer_thread.join();
